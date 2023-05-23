513f3596d8f5b735f1a0987e71726193f4add899
sam-hur <83226062+sam-hur@users.noreply.github.com>
2023-05-24 00:46:13 +0200

    WIP

--- src/Try2/Client/ClientHandler.java
+++ src/Try2/Client/ClientHandler.java
@@ -71,7 +71,7 @@ public class ClientHandler extends Thread {
     }
 
     public void sendMessage(String message) throws IOException {
-        out.write("("+new PersistentTime().getTime().getTime()+") " + message);
+        out.write("("+new PersistentTime().getTime().getHours()+") " + message);
         out.newLine();
         out.flush();
     }
